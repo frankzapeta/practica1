37b473e6305e9c90f92baedf796de4652bd578f8
adiseño.2015 <adiseo.2015@192.168.56.1>
2015-06-08 10:47:12 -0600

    Se agrego la linea de codigo "Hola mundo"

--- src/practica1/Practica1.java
+++ src/practica1/Practica1.java
@@ -15,6 +15,7 @@ public class Practica1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Hola Mundo");
         // TODO code application logic here
     }
     
